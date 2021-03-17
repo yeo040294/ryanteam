3acbebea496871517f709bf5d89ef04564eb2085
Ryan Phuay <Noliferist@ryans-mbp.lan>
2021-03-18 00:14:50 +0800

    added search bar

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,18 @@ import React, { Component } from 'react'
 import Picture from '../components/Picture'
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
 
+
 class Search extends Component {
     render() {
         return (
             <MDBContainer>
-                 <h1>Searching for "Item"</h1>
+                <MDBRow>
+                <form id="searchQuery" action="/action_page.php">
+                    <input type="text" name="query" placeholder="search SecondLove"></input>
+                    <input type="button" onclick="myFunction()" value="Search"></input>
+                </form>
+                </MDBRow>
+                 <h1>Searching for </h1>
                         <h3>XXX Listings found near you</h3>
                 <MDBRow>
                 
