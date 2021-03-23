34f050769da3f631b8f89693cdf3c32f24e4db6c
Noliferist <57478357+Noliferist@users.noreply.github.com>
2021-03-23 12:35:51 +0800

    changed donor name to header

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol, MDBCardHeader, MDBBadge , MDBIcon } from 'mdbreact';
+import Typography from '@material-ui/core/Typography';
 
 const Card = ({ post }) => {
   return (
     <MDBCol>
       <MDBCard style={{ width: "22rem" }}>
+        <MDBCardHeader>
+          <MDBBadge color="grey">
+            <MDBIcon icon="user" />
+          </MDBBadge>
+          <Typography variant="h5" component="h2">
+            {post.userHandle}
+          </Typography>
+        </MDBCardHeader> 
         <MDBCardImage className="img-fluid" src={post.imageUrl} waves />
         <MDBCardBody>
           <MDBCardTitle>{post.itemName}</MDBCardTitle>
@@ -12,8 +21,6 @@ const Card = ({ post }) => {
             Item condition: {post.itemCondition}
             <br/>
             Item ballot status: {post.itemStatus}
-            <br/>
-            Posted by: {post.userHandle}
           </MDBCardText>
           <MDBBtn outline color="pink" /* href={add link to each item here  }*/>View</MDBBtn>
         </MDBCardBody>
