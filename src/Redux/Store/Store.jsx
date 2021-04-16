3df0ce80ee07fe2b06925c41556eee81b65ee2b5
Teo Jia Sheng <jiiasheng@hotmail.com>
2021-04-17 01:28:18 +0800

    removed redux dev tools from store

--- src/Redux/Store/Store.jsx
+++ src/Redux/Store/Store.jsx
@@ -8,11 +8,19 @@ import { reduxFirestore, getFirestore } from 'redux-firestore';
 
 const initialState = {};
 const middleWare = [thunk.withExtraArgument({getFirebase, getFirestore})];
+
+//WITHOUT REDUX DEV TOOLS
 const store = createStore(RootReducer, initialState,
-    compose(applyMiddleware(...middleWare) ,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ,reduxFirestore(firebase)
+    compose(applyMiddleware(...middleWare), reduxFirestore(firebase)
     )
 );
+
+//WITH REDUX DEV TOOLS
+// const store = createStore(RootReducer, initialState,
+//     compose(applyMiddleware(...middleWare) ,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     ,reduxFirestore(firebase)
+//     )
+// );
 
 export default store;
